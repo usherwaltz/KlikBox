f1598db4063d649140f819000d6ee5d23c396226
usherwaltz <usherwalce@gmail.com>
2021-05-09 20:54:29 +0200

    added photo_2 column in blocks table

---
+++ database/migrations/2021_05_09_185152_photo_2_blocks.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Photo2Blocks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blocks', function (Blueprint $table) {
+            $table->string('photo_2', 255)->after('photo')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
