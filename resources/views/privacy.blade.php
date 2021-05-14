caeabae8a56b42e49fee13df519e3d2bdd63f8cf
usherwaltz <usherwalce@gmail.com>
2021-05-14 13:16:49 +0200

    footer tekst update

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,16 +1,8 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @section('css')
-        @parent
-        <link rel="stylesheet" type="text/css" href="/css/style_pp-tu.css">
-    @append
-
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style_pp-tu.css')}}">
+    <title></title>
 </head>
 <body>
 <div class="modal" id="privacy-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
@@ -23,69 +15,46 @@
                 </button>
             </div>
             <div class="modal-body">
-                <p>
-                    Politika privatnosti
+                <div class="page">
 
-                     
-                    Ova Politika privatnosti opisuje kako se lični podaci
-                    prikupljaju, koriste i dijele kada posećujete ili naručujete
-                    preko www.klikbox.ba
-                     
-                    1. KAKO PRIKUPLJAMO LIČNE INFORMACIJE
-                    Kada posjetite Klikbox online prodavnicu, automatski prikupljamo
-                    određene informacije o vašem uređaju, uključujući podatke o
-                    vašem web pretraživaču, IP adresi, vremenskoj zoni i nekim
-                    kolačićima koji su instalirani na vašem uređaju. Pored toga, dok
-                    pregledavate Klikbox online prodavnicu, prikupljamo informacije
-                    o pojedinačnim web stranicama ili proizvodima koje pregledate,
-                    koje web stranice ili pojmovi za pretragu su vas preusmjerili na
-                    Klikbox online prodavnicu, kao i informacije o načinu na koji
-                    komunicirate sa web lokacijom. Ovo automatsko prikupljanje
-                    informacija vršimo pomoću sljedećih tehnologija:
-                    Kolačići su datoteke podataka koje se postavljaju na vaš uređaj
-                    ili računar i često sadrže anonimni jedinstveni identifikator.
-                    Datoteke logovanja evidentiraju akcije koje se događaju na web
-                    lokaciji i prikupljaju podatke uključujući vašu IP adresu, vrstu
-                    pretraživača, datum, vrijeme.
-                    Takođe, kada izvršite narudžbu ili pokušate da izvršite narudžbu
-                    putem Klikbox online prodavnice, od vas prikupljamo određene
-                    podatke, uključujući Vaše ime, poslovnu adresu, Email adresu i
-                    telefonski broj. To se zove Informacije o porudžbini.
-                    2. KAKO KORISTIMO VAŠE LIČNE INFORMACIJE
-                    Informacije o porudžbini koje obično prikupljamo da bismo
-                    ispunili bilo koje porudžbine postavljene preko KlikBox online
+                    <div id="pp" class="pp">
 
-                    prodavnice, koristimo kako bismo Vas kontaktirali i isporučili
-                    željeni proizvod. Takođe, ukoliko se upišete na Newsletter listu,
-                    slaćemo vam informacije, zanimljivosti i promotivne poruke. Sa
-                    mailing liste se u svako doba možete odjaviti.
-                    Pored toga, Vaše korisničko iskustvo koristimo za generalno
-                    poboljšanje i optimizaciju naše KlikBox online prodavnice.
-                    Konekcija sa Klikbox online prodavnicom je osigurana SSL
-                    sertifikatom tako da niko nije u mogućnosti preuzeti Vaše lične
-                    podatke.
-                    3. DIJELJENJE VAŠIH LIČNIH INFORMACIJA
-                    Vaše lične podatke dijelimo sa trećim licima samo u svrhu
-                    praćenja statistika o posjetiocima i proizvodima koji ih zanimaju.
-                    Koristimo Google Analitiku u svrhu analize kako naši klijenti
-                    koriste Klikbox online prodavnicu. Konačno, možemo takođe
-                    dijeliti Vaše lične podatke za potrebe pridržavanja važećih
-                    zakona i propisa, da odgovorimo na sudski poziv, nalog za
-                    pretragu ili druge zakonite zahtjeve.
-                    4. VAŠA PRAVA
-                    Imate pravo da pristupite ličnim podacima koje imamo o Vama i
-                    da zatražite da se isti ispravljaju, ažuriraju ili obrišu. Ako želite da
-                    iskoristite ovo pravo, kontaktirajte nas.
-                    5. POVRATAK PODATAKA
-                    Kada izvršite narudžbu putem KlikBox online prodavnice,
-                    čuvaćemo podatke o Vašoj porudžbini radi naše evidencije, osim
-                    i sve dok ne zatražite da te podatke izbrišemo.
-                    6. PROMJENE
-                    Možemo povremeno ažurirati ovu politiku privatnosti kako bismo
-                    odražavali, na primer, promjene u našoj praksi ili iz drugih
-                    operativnih, pravnih ili regulatornih razloga. Ako imate pitanja i /
-                    ili vam je potrebno više informacija, slobodno nas kontaktirajte.
-                </p>
+                        <h2>Politika privatnosti</h2>
+
+                        <h3>Ova Politika privatnosti opisuje kako se lični podaci prikupljaju, koriste i dijele kada posećujete ili naručujete preko www.klikbox.ba</h3>
+
+                        <h4>1. KAKO PRIKUPLJAMO LIČNE INFORMACIJE</h4>
+
+                        <p>Kada posjetite Klikbox online prodavnicu, automatski prikupljamo određene informacije o vašem uređaju, uključujući podatke o vašem web pretraživaču, IP adresi, vremenskoj zoni i nekim kolačićima koji su instalirani na vašem uređaju. Pored toga, dok pregledavate Klikbox online prodavnicu, prikupljamo informacije o pojedinačnim web stranicama ili proizvodima koje pregledate, koje web stranice ili pojmovi za pretragu su vas preusmjerili na Klikbox online prodavnicu, kao i informacije o načinu na koji komunicirate sa web lokacijom. Ovo automatsko prikupljanje informacija vršimo pomoću sljedećih tehnologija: <br><br>
+                            Kolačići su datoteke podataka koje se postavljaju na vaš uređaj ili računar i često sadrže anonimni jedinstveni identifikator.
+                            Datoteke logovanja evidentiraju akcije koje se događaju na web lokaciji i prikupljaju podatke uključujući vašu IP adresu, vrstu pretraživača, datum, vrijeme. <br><br>
+                            Takođe, kada izvršite narudžbu ili pokušate da izvršite narudžbu putem Klikbox online prodavnice, od vas prikupljamo određene podatke, uključujući Vaše ime, poslovnu adresu, Email adresu i telefonski broj. To se zove Informacije o porudžbini</p>
+
+                        <h4>2. KAKO KORISTIMO VAŠE LIČNE INFORMACIJE</h4>
+
+                        <p>Informacije o porudžbini koje obično prikupljamo da bismo ispunili bilo koje porudžbine postavljene preko KlikBox online prodavnice, koristimo kako bismo Vas kontaktirali i isporučili željeni proizvod. Takođe, ukoliko se upišete na Newsletter listu, slaćemo vam informacije, zanimljivosti i promotivne poruke. Sa mailing liste se u svako doba možete odjaviti. <br><br>
+                            Pored toga, Vaše korisničko iskustvo koristimo za generalno poboljšanje i optimizaciju naše KlikBox online prodavnice. <br><br>
+                            Konekcija sa Klikbox online prodavnicom je osigurana SSL sertifikatom tako da niko nije u mogućnosti preuzeti Vaše lične podatke.
+                        </p>
+
+                        <h4>3. DIJELJENJE VAŠIH LIČNIH INFORMACIJA</h4>
+
+                        <p>Vaše lične podatke dijelimo sa trećim licima samo u svrhu praćenja statistika o posjetiocima i proizvodima koji ih zanimaju. Koristimo Google Analitiku u svrhu analize kako naši klijenti koriste Klikbox online prodavnicu. Konačno, možemo takođe dijeliti Vaše lične podatke za potrebe pridržavanja važećih zakona i propisa, da odgovorimo na sudski poziv, nalog za pretragu ili druge zakonite zahtjeve.</p>
+
+                        <h4>4. VAŠA PRAVA</h4>
+
+                        <p>Imate pravo da pristupite ličnim podacima koje imamo o Vama i da zatražite da se isti ispravljaju, ažuriraju ili obrišu. Ako želite da iskoristite ovo pravo, kontaktirajte nas.</p>
+
+                        <h4>5. POVRATAK PODATAKA</h4>
+
+                        <p>Kada izvršite narudžbu putem KlikBox online prodavnice, čuvaćemo podatke o Vašoj porudžbini radi naše evidencije, osim i sve dok ne zatražite da te podatke izbrišemo.</p>
+
+                        <h4>6. PROMJENE</h4>
+
+                        <p>Možemo povremeno ažurirati ovu politiku privatnosti kako bismo odražavali, na primer, promjene u našoj praksi ili iz drugih operativnih, pravnih ili regulatornih razloga. Ako imate pitanja i / ili vam je potrebno više informacija, slobodno nas kontaktirajte.</p>
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
