f95dbe5b73eba97384bada39b0ddeb7c0ff3f5be
usherwaltz <usherwalce@gmail.com>
2021-05-26 13:14:26 +0200

    updates

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -13,9 +13,11 @@
 @if ($errors->any())
     <div class="alert alert-danger">
         <ul>
+
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
+
         </ul>
     </div>
 @endif
